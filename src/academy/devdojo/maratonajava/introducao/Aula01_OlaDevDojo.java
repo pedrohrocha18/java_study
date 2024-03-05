e6a0ff1269618fd6df8ea3ebf1160ab46f787ce0
pedrohrocha18 <phr.tech@outlook.com>
2024-03-05 17:31:30 -0300

    update

--- src/academy/devdojo/maratonajava/introducao/Aula01_OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01_OlaDevDojo.java
@@ -1,4 +1,5 @@
-public class OlaDevDojo {
+package academy.devdojo.maratonajava.introducao;
+public class Aula01_OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02_TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02_TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 18;
+        System.out.println("A idade é: "+ age + " anos");
+    }
+}
