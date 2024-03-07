48ce5076ec63f9cf67a64800b577bbac58401ca4
pedrohrocha18 <phr.tech@outlook.com>
2024-03-07 09:24:59 -0300

    update study

--- src/academy/devdojo/maratonajava/introducao/Aula02_TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02_TiposPrimitivos.java
@@ -4,7 +4,7 @@ public class Aula02_TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
         int age = 29;
-        long largerNumber = 150000;
+/*        long largerNumber = 150000;
         double salaryDouble = 1.800;
         float salaryFloat = 2500;
         byte salaryByte = 127;
@@ -12,6 +12,7 @@ public class Aula02_TiposPrimitivos {
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 10; //unicode
+*/
         String name = "Pedro";
 
         System.out.println("Hello! My name is "+ name + ". I have " + age + " years.");
