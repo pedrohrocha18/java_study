3834155c03b4138e20608ca73b3e4cf6f6308d29
pedrohrocha18 <phr.tech@outlook.com>
2024-03-08 17:38:39 -0300

    update study = operadores lógicos

--- src/academy/devdojo/maratonajava/introducao/Aula04_Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04_Operadores.java
@@ -11,20 +11,36 @@ public class Aula04_Operadores {
         // ' >= <= ' maior igual, menor igual
         // ' == != ' igual, diferente
 
-        double numb1 = 10;
+/*        double numb1 = 10;
         double numb2 = 20;
         boolean isDezIgualVinte = numb1 == numb2;
         boolean isDezIgualDez = numb1 == numb1;
         boolean isDezMaiorQueVinte = numb1 > numb2;
         boolean isDezMenorQueVinte = numb1 < numb2;
         boolean isDezDiferenteDeVinte = numb1 != numb2;
-
         System.out.println("Dez igual a vinte? " + isDezIgualVinte);
         System.out.println("Dez igual a dez? " + isDezIgualDez);
         System.out.println("Dez maior que vinte? " + isDezMaiorQueVinte);
         System.out.println("Dez menor que vinte? " + isDezMenorQueVinte);
-        System.out.println("Dez diferente de vinte? " + isDezDiferenteDeVinte);
+        System.out.println("Dez diferente de vinte? " + isDezDiferenteDeVinte);*/
 
+        // && => todas as condições precisam ser verdadeiras
+        // || => uma das condições precisa ser verdadeira
+        // ! => operador de negação "ao contrário"
+
+        double saldoContaCorrente = 140;
+        double saldoContaPoupança = 3500;
+        float valorPlaystation = 5000;
+
+        if (saldoContaCorrente == 5000 || saldoContaPoupança == 5000) {
+            System.out.println("Compra realizada com sucesso!");
+        } else if (saldoContaCorrente <= 5000 || saldoContaPoupança == 5000) {
+            System.out.println("Saldo insuficiente para realizar a compra!");
+        } else if (saldoContaCorrente + saldoContaPoupança >= 5000) {
+            System.out.println("Compra realizada com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente para realizar a compra!");
+        }
 
     }
 }
